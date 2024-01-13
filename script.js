0020f830942cd5acf99376de5f67c042fa550628
KIRTANDEL <98909384+TANDEL1@users.noreply.github.com>
2024-01-13 16:07:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	let index = 0;
+            for(let i=0; i<s.length; i++)
+            {
+                if(s.charAt(i) == " ")
+                {
+                    index = i;
+                    break;
+                }
+            } 
+            return s.slice(0, index);
 }
 
 // Do not change the code below
